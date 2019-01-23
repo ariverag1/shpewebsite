44995d44468b69383c8ee451f3d61f28e35c38cc
ariverag1 <43007243+ariverag1@users.noreply.github.com>
2019-01-22 19:32:38 -0800

    added js file

---
+++ logonnavi.js
@@ -0,0 +1,15 @@
+﻿ document.getElementById("logonnavi").innerHTML =
+
+
+
+'<div id="bannerimage">' +    
+'</div>' +
+    
+'<!-- 2. Div for the navigation bar -->' +
+'<div id="menu">' +
+    '<ul>' +
+        '<li class="current_page_item"><a href="/index.html" accesskey="1" title="">Home</a></li>' +
+        '<li><a href="/next.html" accesskey="2" title="">History</a></li>' +
+    '</ul>' +
+'</div>';
+
